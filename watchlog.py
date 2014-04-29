7316fc72569cf743e0954d7ee259c4f3cde9b109
Paul Vacey <vaks0731@gmail.com>
2014-04-29 22:37:33 +0000

    I think the IP grabber part works perfectly now

--- watchlog.py
+++ watchlog.py
@@ -1,53 +1,103 @@
 #!venv/bin/python
 
+
+#Need to handle log rotation --> "if offset too high then zero"
+#			     --> same IP over different logs files?
+#				 --> store offset of ip.txt also to keep master list of IPs
+#				 --> add new IPs to the list then come back to geolocate just like 
+#				     it does to find the IPs in the first place
+#				     --> this should work if getNewIP first then geolocate second
+
 import time, re
 directory = '/home/paul/projects/geossh/'
 
 def getNewIPs():
-	ftell = open (directory+'tell.txt')
+	#open file that holds offset (tell) value
+	ftell = open (directory+'offset.txt')
 	ftell.seek(0.0)
-	offset = int(ftell.read()) 
+	offset = int(ftell.readline()) 
+	#ftell.close()
+
 	print "tell.txt --> "+ str(offset)
 
+	#open the log at the offset point
 	flog = open (directory+'auth.log') #make a symlink to directory
-	#open the log at the offset
+	#find current endpoint of file
+	flog.seek(0,2)
+	logLength = flog.tell()
+	#if current endpoint is less than old one, start at 0 again (new log file)
+	if logLength < offset:
+		offset = 0
+	
+	print "starting log from --> " + str(offset)
+	
+	
 	flog.seek(offset,0)
 	tmp = flog.read()
+	newOffset = str(flog.tell())
+	flog.close()
 
-	#save the new end of the log
-	print "end of log --> "+  str(flog.tell())
-	#re-open to overwrite file
+	print "end of log --> "+  newOffset
+
+
+
+
+	#this also checks to see where the end of ip.txt file is, and starts fresh if past end	
+	ipOffset = ftell.readline() 
+	print ipOffset
+	fip = open (directory+'ip.txt')
+	fip.seek(0,2)
+	ipLogLength = fip.tell()
+	if ipOffset > ipLogLength:
+		fip.close()
+		reOpen = True
+		#fip = open(directory+'ip.txt','w')
+		ipOffset = 0
+	if reOpen:
+		fip = open(directory+'ip.txt','r+')
+
+	print "start ip.txt -->" + str(ipOffset)
+	fip.seek(int(ipOffset),0)
+	oldIP = fip.read()
+	ipOffset = str(fip.tell()) 	
+
+	#overwrite the offset value
+	ftell = open (directory+'offset.txt','w')
+	ftell.write(newOffset+"\n")
+	ftell.write(ipOffset)
 	ftell.close()
-	ftell = open (directory+'tell.txt','w')
-	ftell.write(str(flog.tell()))
 
-
-	#DO NOT NEED ANYMORE I THINK
-
-	#fip = open (directory+'ip.txt')
-	#fip.seek(0.0)
-	#oldIP = fip.read()
-
+	#grab the IPs from the log
 	tmp = re.findall("Invalid .+ from (.+)", tmp)
 
 	ipList = []
-	#start as blank
 	previous = ""
+	#remove duplicates
 	for ip in sorted(tmp):
-		#make sure this isnt the string as before it
+		#make sure each ip is different from the one before it
 		if ip != previous:
 			ipList.append(ip)	
 			previous = ip
 
-	#oldIP = oldIP.split()
-	#newIpList = list(set(ipList) - set(oldIP))  
 
+	#
+	#Need to fix this so that it does not add duplicates
+	#
+	oldIP = oldIP.split()
+	print oldIP
+	newIpList = list(set(ipList) - set(oldIP))  
 	#newIpList = ipList 
 
-	print ipList
+
+	print newIpList
+
+	#!!!!!
+	#Can probably just keep this as a list...?
+	#!!!!!!
+
 	#write only the new ip address to the file
-	outpoot =  open(directory+'ip.txt' , 'w')
-	for ip in ipList:
+	outpoot =  open(directory+'ip.txt' , 'a')
+	for ip in newIpList:
 		outpoot.write(ip+"\n")
 	outpoot.close()
 	return
